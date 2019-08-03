136cd9ff46df2542aab44034bb7e12f1b4e24f70
JP Bureik <jp.bureik@protonmail.com>
2019-08-03 15:10:38 +0200

    implemented properties for vocable class attributes

--- vocable.py
+++ vocable.py
@@ -13,21 +13,36 @@ class Vocable:
     """
     def __init__(self, language, native, foreign):
         self._language = language
-        self.phase = 0
-        self.date = date.today()
-        self.native = native
-        self.foreign = foreign
+        self._phase = 0
+        self._date = date.today()
+        self._native = native
+        self._foreign = foreign
+        
+    @property
+    def language(self):
+        return self._language
+        
+    @property
+    def phase(self):
+        return self._phase
+        
+    @property
+    def date(self):
+        return self._date
     
-    def edit(self):
-        ''' Edit any public attribute of vocable class instance.'''
-        select = input('Specify attribute to change:\n')
-        try:
-            print('Current: ' + str(getattr(self, select)))
-            change = input('Edit: ')
-            setattr(self, select, change)
-            print('Changed ' + select + ' to ' + change + '.')
-        except:
-            print('Attribute not found')
+    @property
+    def native(self):
+        return self._native
     
-    def practice(self):
-        pass
+    @native.setter
+    def native(self, input_string):
+        self._native = input_string
+        
+    @property
+    def foreign(self):
+        return self._foreign
+    
+    @foreign.setter
+    def foreign(self, input_str):
+        self._foreign = input_str
+    
