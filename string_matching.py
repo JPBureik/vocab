99374e0df5635cc93bfe932a8740f8aae1640f61
JP Bureik <jp.bureik@protonmail.com>
2020-05-01 20:36:07 +0200

    Implemented pep8 conformity

--- string_matching.py
+++ string_matching.py
@@ -15,22 +15,23 @@ import pandas as pd
 from termcolor import colored
 import terminal_commands as tc
 
+
 def string_matching(input_str, column_name, h5_file):
 
     database = pd.read_hdf(h5_file, 'df')
-    if database[database[column_name].str.contains(input_str)].empty == False:
+    if database[database[column_name].str.contains(input_str)].empty is False:
         print(colored('Duplicate warning:', 'red'))
         print(database[database[column_name].str.contains(input_str)])
         proceed = input('Proceed? [y/n]\n')
         if proceed == 'y':
             dupl = False
-             # Delete lines of duplicate list
-            tc.del_lines(len(database[database[column_name].str.contains\
-                                     (input_str)])+4)
+            # Delete lines of duplicate list
+            tc.del_lines(len(database[database[column_name].str.contains
+                                      (input_str)])+4)
         elif proceed == 'n':
             dupl = True
             # Delete lines of duplicate list
-            tc.del_lines(len(database[database[column_name].str.\
-                                     contains(input_str)])+6)
+            tc.del_lines(len(database[database[column_name].str.contains
+                                      (input_str)])+6)
 
     return dupl
--- vocable.py
+++ vocable.py
@@ -5,8 +5,8 @@ Created on Mon Jul 29 01:59:10 2019
 
 @author: JP
 
-This class defines the Vocable class including methods for creating, editing and
-practicing vocable items.
+This class defines the Vocable class including methods for creating, editing
+and practicing vocable items.
 """
 
 # Standard library imports
@@ -16,11 +16,10 @@ from tabulate import tabulate
 from termcolor import colored
 
 # Local application imports
-import terminal_commands as tc # Use tc.del_lines(int) to delete int previous
-                               # lines in terminal
-import string_matching as sm # Check for duplicates during input
+import terminal_commands as tc  # Use tc.del_lines(int) to delete int previous
+# lines in terminal
+import string_matching as sm  # Check for duplicates during input
 
-#%%
 
 class Vocable:
     """
@@ -31,20 +30,19 @@ class Vocable:
         foreign_item(str): vocable item in foreign language
     """
 
-
     """
     TOP-LEVEL PRIVATE ATTRIBUTES
     """
 
     # Modifiable class variables
     _native_language = 'German'
-    _language_choice = ['English', 'French']    # Foreign languages in which you
-                                                # have vocab items
-    _num_for_practice = 100 # Max number of cards for daily practice
+    _language_choice = ['English', 'French']    # Foreign languages in which
+    # you have vocab items
+    _num_for_practice = 100  # Max number of cards for daily practice
 
     # Initialize class variables needed for methods
     _set_of_cards = set()   # This is your entire library of vocab items in a
-                            # given foreign language
+    # given foreign language
 
     """
     INIT
@@ -56,7 +54,6 @@ class Vocable:
         self._date = date.today()
         self._native = native
         self._foreign = foreign
-
 
     """
     PRIVATE METHODS
@@ -81,23 +78,24 @@ class Vocable:
     def __overview(cls):
         table_list = []
         header_column = cls._foreign_language
-        header_line = ['','Total', 'Phase 0', 'Phase 1','Phase 2','Phase 3',\
-                       'Phase 4','Phase 5', 'LTM', 'Last practiced']
+        header_line = ['', 'Total', 'Phase 0', 'Phase 1', 'Phase 2', 'Phase 3',
+                       'Phase 4', 'Phase 5', 'LTM', 'Last practiced']
         table_list.append(header_column)
         table_list.append(len(cls._df))
-        for l in range(1,8,1):
-            table_list.append(len(cls._df.loc[cls._df['Phase'] == l-1,\
+        for l in range(1, 8, 1):
+            table_list.append(len(cls._df.loc[cls._df['Phase'] == l-1,
                                               cls._foreign_language]))
         table_list.append(max(cls._df['Date']))
         # Print table
-        print(tabulate([table_list],headers=header_line,tablefmt='fancy_grid',\
-                       numalign='center',stralign = 'center'))
+        print(tabulate([table_list], headers=header_line,
+                       tablefmt='fancy_grid', numalign='center',
+                       stralign='center'))
 
     # Main menu: present choice of all other public class methods after setup
     @classmethod
     def __main_menu(cls):
         print('MAIN MENU')
-        selection = int(input('Select action: 0 = Input, 1 = Edit, 2 = '\
+        selection = int(input('Select action: 0 = Input, 1 = Edit, 2 = '
                               'Practice\n'))
         # Delete input from terminal
         tc.del_lines(1)
@@ -108,11 +106,9 @@ class Vocable:
         else:
             pass
 
-
     """
     PUBLIC METHODS
     """
-
 
     ''' Initialization: Choose foreign language, display overview, then enter
     main menu
@@ -120,9 +116,9 @@ class Vocable:
     @classmethod
     def initialize(cls):
         # Select foreign language and store choice in class variable
-        selection = int(input('Select language: 0 = ' +\
-                                          cls._language_choice[0] + ', 1 = ' +\
-                                          cls._language_choice[1] + ':\n'))
+        selection = int(input('Select language: 0 = ' +
+                              cls._language_choice[0] + ', 1 = ' +
+                              cls._language_choice[1] + ':\n'))
         cls._foreign_language = cls._language_choice[selection]
         # Delete input from terminal
         tc.del_lines(1)
@@ -140,6 +136,9 @@ class Vocable:
     @classmethod
     def input_loop(cls):
 
+        # Add new entries to DataFrame and save
+        # def
+
         # Define local aux functions
         def foreign_input():
             # Change enclosed variable to exit input function
@@ -147,14 +146,15 @@ class Vocable:
             # Update enclosed counter variable
             nonlocal counter
             # Input new vocab item: answer
-            foreign_input_str = input('Enter question [' +\
-                cls._foreign_language + ']:\n')
+            foreign_input_str = input('Enter question [' +
+                                      cls._foreign_language + ']:\n')
             # Check for exit command
             if foreign_input_str != 'q':
                 # Check for duplicates
-                dupl_foreign = sm.string_matching(foreign_input_str,\
-                    cls._foreign_language, cls._vocab_file)
-                if dupl_foreign == False:
+                dupl_foreign = sm.string_matching(foreign_input_str,
+                                                  cls._foreign_language,
+                                                  cls._vocab_file)
+                if dupl_foreign is False:
                     # Update progress counter
                     counter += 1
                     # Delete lines from previous input
@@ -170,7 +170,7 @@ class Vocable:
                     else:
                         print('{} new entries added.'.format(counter))
                 else:
-                    foreign_input() # Recursion
+                    foreign_input()  # Recursion
             else:
                 # Delete lines from previous input
                 tc.del_lines(9)
@@ -180,17 +180,18 @@ class Vocable:
             # Change enclosed variable to exit input function
             nonlocal continue_input
             # Input new vocab item: question
-            native_input_str = input('Enter question [' +\
-                cls._native_language + ']:\n')
+            native_input_str = input('Enter question [' +
+                                     cls._native_language + ']:\n')
             # Check for exit command
             if native_input_str != 'q':
                 # Check for duplicates
-                dupl_native = sm.string_matching(native_input_str,\
-                    cls._native_language, cls._vocab_file)
-                if dupl_native == False:
+                dupl_native = sm.string_matching(native_input_str,
+                                                 cls._native_language,
+                                                 cls._vocab_file)
+                if dupl_native is False:
                     foreign_input()
                 else:
-                    native_input() # Recursion
+                    native_input()  # Recursion
             else:
                 # Delete lines from previous input
                 tc.del_lines(7)
@@ -199,10 +200,10 @@ class Vocable:
         # Confirm selection choice
         print('You have selected INPUT. Enter q to exit.\n')
 
-        counter = 0 # Variable that indicates the progress
-        continue_input = True # Variable that exits the input function
+        counter = 0  # Variable that indicates the progress
+        continue_input = True  # Variable that exits the input function
 
-        while continue_input == True:
+        while continue_input is True:
             native_input()
 
         # Return to main menu after input
@@ -227,7 +228,7 @@ class Vocable:
                 print('Incorrect!\n')
 
 
-#%% EXECUTE
+# %% EXECUTE
 
 if __name__ == "__main__":
     Vocable.initialize()
