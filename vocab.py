03118b06c17597beb3c98da5fb7bb5ad8be8d88a
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-07-14 13:44:41 +0200

    New start

--- vocab.py
+++ vocab.py
@@ -5,16 +5,79 @@ Created on Mon Feb 10 23:59:24 2020
 
 @author: JP
 
-This script uses the Vocable class in vocable.py to execute its methods for
-creating, editing and practicing vocable items.
+Vocable class.
+
 """
 
 # Standard library imports
+from datetime import date
 
 
-# Local application imports
-from vocable import Vocable
+class Vocable:
+    """
+    A vocable card and methods for input, editing and practice.
 
-#%% Launch vocab programm
+    Init args:
+        native_item(str): vocable item in native language
+        foreign_item(str): vocable item in foreign language
+    """
 
-Vocable.initialize()
+    """
+    TOP-LEVEL PRIVATE ATTRIBUTES
+    """
+
+    _phase_intervals = [0, 1, 3, 9, 29, 90, 300]  # days before next practice]
+    _phases = dict(zip(range(len(_phase_intervals)), _phase_intervals))
+    _native_lang = 'German'
+    _foreign_lang_choice = ['English', 'French']
+    _practice_items = 100  # per practice session and foreign language
+
+    """
+    INIT
+    """
+
+    def __init__(self, foreign_lang, foreign, native):
+        self._foreign_lang = foreign_lang
+        self._foreign = foreign
+        self._native = None
+        self._last_pract_date = date.today()
+        self._phase = 0
+        self._practiced_ctr = 0
+        self._false_ctr = 0
+
+    """
+    PRIVATE METHODS
+    """
+
+    def _pull_from_leo_org(self):
+        ...
+
+    def _pull_from_dict_cc(self):
+        ...
+
+    def _enter_manually(self):
+        ...
+
+    def _load_vocab(self):
+        ...
+
+    """
+    PUBLIC METHODS
+    """
+
+    @classmethod
+    def input(cls):
+        ...
+
+    @classmethod
+    def edit(cls):
+        ...
+
+    @classmethod
+    def practice(cls):
+        ...
+
+
+# Execution:
+if __name__ == '__main__':
+    vocable_card = Vocable('English', 'lake', 'See')
