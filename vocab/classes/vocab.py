dd30e7ac3f21ef84147a57ef26983c5604b777ad
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-09-13 21:54:10 +0200

    Created Vocab class

---
+++ vocab/classes/vocab.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 21:52:28 2021
+
+@author: jp
+
+Vocab class.
+
+A class for training vocab.
+"""
+
+
+class Vocab():
+
+
+    def __init__(self):
+        pass
