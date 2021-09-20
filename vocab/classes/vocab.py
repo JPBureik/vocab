330e63af9834b9eab359d01571d8c7bd222fd519
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-09-21 01:07:01 +0200

    sel_for_practice method for VocabCollection class

--- vocab/classes/vocab.py
+++ vocab/classes/vocab.py
@@ -13,9 +13,10 @@ A class for training vocab.
 # Standard library imports:
 import os
 import pandas as pd
+import datetime
 
 # Local imports:
-from vocab.config import *
+from vocab.config import native_language, daily_pract_amt
 
 
 class VocabCollection():
@@ -26,7 +27,8 @@ class VocabCollection():
         self.native_lang = native_language.lower()
         self.foreign_language = foreign_language.lower()
         self._daily_pract_amt = daily_pract_amt
-        self._phase_intvls = dict(enumerate([0, 1, 3, 9, 29, 90, 300])) # days before next practice
+        # Phases = Days before next practice:
+        self._phase_intvls = dict(enumerate([0, 1, 3, 9, 29, 90, 300]))
         # File I/O:
         self._data_dir = os.getcwd() + '/data'
         self._filepath = self._data_dir + '/' + self.foreign_language + '.h5'
@@ -54,7 +56,15 @@ class VocabCollection():
 
     def _sel_for_practice(self):
         """Select daily_pract_amt from vocab collection."""
-        pass
-
+        self._df_pract = self._df.copy()
+        # Determine items eligible for practice based on their phase:
+        for i in range(len(self._df)):
+            # Discard all | today - last practiced date < phase intvl:
+            if ((datetime.date.today() - self._df.Date.iloc[i]).days <
+                    self._phase_intvls[self._df.Phase.iloc[i]]):
+                self._df_pract.drop(i, inplace=True)
+        # Randomly select defined amount for daily practice:
+        if len(self._df_pract) > self._daily_pract_amt:
+            self._df_pract = self._df_pract.sample(self._daily_pract_amt)
 
     """ ---------- PUBLIC METHODS ---------- """
